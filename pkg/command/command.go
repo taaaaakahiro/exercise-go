9aea564ee07c25e52c26b03beff52e5e73fdb38e
takahiro <takapi854@gmail.com>
2022-06-01 17:43:47 +0900

    update command

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"os"
 	"context"
+
+	"exercise-go-api/pkg/version"
+
+	"go.uber.org/zap"
 )
 
 const (
@@ -12,10 +16,16 @@ const (
 )
 
 func Run() {
-	fmt.Println("start")
 	os.Exit(run(context.Background())) // 空のコンテキスト生成	
 }
 
 func run(ctx context.Context) int {
-	return
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to setup logger: %s\n", err)
+		return exitError
+	}
+	defer logger.Sync()
+	logger = logger.With(zap.String("version", version.Version))
+	return exitOk
 }

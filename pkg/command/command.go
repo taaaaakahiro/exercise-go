4849724c1d72f2d9a586a87892a5978081247c4b
takahiro <takapi854@gmail.com>
2022-06-02 10:45:55 +0900

    update  *.go files

--- pkg/command/command.go
+++ pkg/command/command.go
@@ -5,11 +5,16 @@ import (
 	"os"
 	"context"
 	"net"
+	"os/signal"
+	"syscall"
 
 	"exercise-go-api/pkg/version"
 	"exercise-go-api/pkg/config"
+	"exercise-go-api/pkg/server"
+	"exercise-go-api/pkg/handler"
 
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 const (
@@ -28,7 +33,7 @@ func run(ctx context.Context) int {
 		fmt.Fprintf(os.Stderr, "failed to setup logger: %s\n", err)
 		return exitError
 	}
-	defer logger.Sync()
+	defer logger.Sync() //関数を抜ける際にログをフラッシュしてバッファに残っているログレコードを書き込む
 	logger = logger.With(zap.String("version", version.Version))
 
 	// load config
@@ -44,10 +49,69 @@ func run(ctx context.Context) int {
 		logger.Error("failed to listen port", zap.Int("port", cfg.Port), zap.Error(err))
 		return exitError
 	}
-	logger.Info("server start listening", zap.Int("port", cfg.Port)) // ClIにログを書き出す
+	logger.Info("server start listening", zap.Int("port", cfg.Port)) // ログを書き出す
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
+
+	// init mongo db
+	// logger.Info("connect to mongo db", zap.String("url", cfg.DB.URL), zap.String("source", cfg.DB.Source))
+	// opts := &options.ClientOptions{}
+	// if cfg.DB.Source == "external" {
+	// 	opts = options.Client().SetAuth(options.Credential{AuthMechanism: "MONGODB-AWS", AuthSource: "$external"})
+	// }
+
+	// mongoClient, err := mongo.NewClient(options.Client().ApplyURI(cfg.DB.URL), opts)
+	// if err != nil {
+	// 	logger.Error("failed to create mongo db client", zap.Error(err), zap.String("uri", cfg.DB.URL))
+	// 	return exitError
+	// }
+
+	// mongoCtx, mongoCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer mongoCancel()
+
+	// if err := mongoClient.Connect(mongoCtx); err != nil {
+	// 	logger.Error("failed to connect to mongo db", zap.Error(err))
+	// 	return exitError
+	// }
+
+	// if err := mongoClient.Ping(mongoCtx, readpref.Primary()); err != nil {
+	// 	logger.Error("failed to ping mongo db", zap.Error(err))
+	// 	return exitError
+	// }
+	// mongoDB := mongoClient.Database(cfg.DB.Database)
+
+	// get repositories
+	// repositories, err := persistence.NewRepositories(mongoDB)
+	// if err != nil {
+	// 	logger.Error("failed to new repositories", zap.Error(err))
+	// 	return exitError
+	// }
+
+	// init to start http server
+	registry := handler.NewHandler(logger, repositories, version.Version)
+	httpServer := server.NewServer(registry, &server.Config{Log: logger})
+	wg, ctx := errgroup.WithContext(ctx)
+	wg.Go(func() error {
+		return httpServer.Serve(listener)
+	})
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, os.Interrupt)
+	select {
+	case <-sigCh:
+	case <-ctx.Done():
+	}
+
+	if err := httpServer.GracefulShutdown(ctx); err != nil {
+		return exitError
+	}
+
+	cancel()
+	if err := wg.Wait(); err != nil {
+		return exitError
+	}
+
 
 	return exitOk
 
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,1 +1,30 @@
 package handler
+
+import (
+	"exercise-go-api/pkg/handler/v1"
+	"exercise-go-api/pkg/handler/version"
+	"go.uber.org/zap"
+)
+
+type Handler struct {
+	V1      *v1.Handler
+	// Version *version.Handler
+}
+
+// func NewHandler(logger *zap.Logger, repo *persistence.Repositories, ver string) *Handler {
+// 	h := &Handler{
+// 		V1:      v1.NewHandler(logger.Named("v1"), repo),
+// 		Version: version.NewHandler(logger.Named("version"), ver),
+// 	}
+
+// 	return h
+// }
+
+func NewHandler(logger *zap.Logger) *Handler {
+	h := &Handler{
+		V1:      v1.NewHandler(logger.Named("v1"), repo),
+		// Version: version.NewHandler(logger.Named("version"), ver),
+	}
+
+	return h
+}
